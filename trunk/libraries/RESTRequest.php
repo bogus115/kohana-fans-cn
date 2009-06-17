d42b2c0eec978bf196a5fadde3da48718601339e
icyleaf.cn@gmail.com <icyleaf.cn@gmail.com@133ad5d2-03c2-11de-ada4-5153b8187bf2>
2009-06-17 14:47:10 +0000

    Updated OAuth library.

--- trunk/libraries/RESTRequest.php
+++ trunk/libraries/RESTRequest.php
@@ -1,26 +1,26 @@
-<?php
-
+<?php defined('SYSPATH') or die('No direct script access.');
 /**
- * undocumented class
+ * RESTRequest class
  *
- * @package default
- * @author Matt Wells (www.ninjapenguin.co.uk)
+ * @package libraries
+ * @author icyleaf
+ * @version 0.2
  *
- * @cracked by icyleaf. 
- * Edited the secend param of Curl::post()
+ * Base on Curl Library by Matt Wells (www.ninjapenguin.co.uk)
+ *
  **/
-class Curl_Core {
-	
-	private $resource = null;
+class RESTRequest_Core {
 	
 	private $config = array();
+	private $resource = null;
+	private static $http_code = null;
 	
 	/**
 	 * Factory Method
 	 */
 	public static function factory($data = array())
 	{
-		return new Curl($data);
+		return new RESTRequest($data);
 	}
 	
 	/**
@@ -45,7 +45,6 @@ class Curl_Core {
 		foreach ($this->config as $key => $value) {
 			$this->set_opt($key, $value);
 		}
-		
 	}
 	
 	/**
@@ -60,7 +59,6 @@ class Curl_Core {
 		return $this;
 	}
 	
-	
 	/**
 	 * Execute the curl request and return the response
 	 * @return String				Returned output from the requested resource
@@ -71,11 +69,15 @@ class Curl_Core {
 		$ret = curl_exec($this->resource);
 		
 		//Wrap the error reporting in an exception
-		if($ret === false) 
+		if($ret === false)
+		{
 			throw new Kohana_User_Exception("Curl Error", curl_error($this->resource));
+		}
 		else
+		{
+			self::$http_code = curl_getinfo($this->resource, CURLINFO_HTTP_CODE);
 			return $ret;
-		
+		}
 	}
 	
 	/**
@@ -107,18 +109,21 @@ class Curl_Core {
 	 */
 	public static function get($url, Array $headers = array(), $headers_only = false, Array $curl_options = array())
 	{
-		$ch = Curl::factory($curl_options);
+		$ch = RESTRequest::factory($curl_options);
 		
 		$ch->set_opt(CURLOPT_URL, $url)
-      	->set_opt(CURLOPT_RETURNTRANSFER, true)
+		->set_opt(CURLOPT_RETURNTRANSFER, true)
 		->set_opt(CURLOPT_NOBODY, $headers_only);
 		
 		//Set any additional headers
-		if(!empty($headers)) $ch->set_opt(CURLOPT_HTTPHEADER, $headers);
+		if( !empty($headers) ) 
+			$ch->set_opt(CURLOPT_HTTPHEADER, $headers);
 		
-		return $ch->exec();
+		$result['content'] = $ch->exec();
+		$result['http_code'] = self::$http_code;
+		
+		return new RESTResponse($result);
 	}
-	
 	
 	/**
 	 * Post
@@ -131,7 +136,7 @@ class Curl_Core {
 	 */
 	public static function post($url, $data, Array $headers = array(), $headers_only = false, Array $curl_options = array())
 	{
-		$ch = Curl::factory($curl_options);
+		$ch = RESTRequest::factory($curl_options);
 		
 		$request = '';
 		if ( is_array($data) )
@@ -149,12 +154,61 @@ class Curl_Core {
 		->set_opt(CURLOPT_RETURNTRANSFER, true)
 		->set_opt(CURLOPT_POST, true)
 		->set_opt(CURLOPT_POSTFIELDS, $request);
-      
-      	//Set any additional headers
-		if(!empty($headers)) $ch->set_opt(CURLOPT_HTTPHEADER, $headers);
+	  
+		//Set any additional headers
+		if( !empty($headers) ) 
+			$ch->set_opt(CURLOPT_HTTPHEADER, $headers);
 		
-	    return $ch->exec();
+		$result['content'] = $ch->exec();
+		$result['http_code'] = self::$http_code;
+		
+		return new RESTResponse($result);
 	}
 }
 
+/**
+ * RESTResponse class
+ *
+ * @package libraries
+ * @author icyleaf
+ * @version 0.1
+ *
+ **/
+class RESTResponse {
+	
+	private $content;
+	private $http_code;
+	
+	public function __construct($response)
+	{
+		$this->content = $response['content'];
+		$this->http_code = $response['http_code'];
+		
+		return $this->content;
+	}
+	
+	public function to_nomral_format()
+	{
+		return $this->content;
+	}
+	
+	public function to_xml()
+	{
+		return $this->content;
+	}
+	
+	public function to_json()
+	{
+		return json_decode($this->content, true);
+	}
+	
+	public function get_http_code()
+	{
+		return $this->http_code;
+	}
+	
+	public function __toString() {
+		return (string) $this->get_http_code();
+	}
+}
 ?>
